c311a55f5b2c79f22762f3e8cc296ce210c52575
Philip-21 <philipuzomaobiora@gmail.com>
2023-11-26 00:12:49 +0100

    handlers added and created
    Signed-off-by: Philip-21 <philipuzomaobiora@gmail.com>

---
+++ UserService/handlers/handlers.go
@@ -0,0 +1,59 @@
+package handlers
+
+import (
+	"log"
+	"net/http"
+	"user/config"
+	"user/database"
+
+	"github.com/gin-contrib/sessions"
+	"github.com/gin-gonic/gin"
+)
+
+type Repository struct {
+	app *config.AppConfig
+}
+
+//A constructor function for the Repository struct, it initializes the Repository struct with the app variable, 
+//making the app accessible within the Repository instance.
+//This instance can then be used to initialize and run all your handlers
+//This pattern helps in keeping your code modular and makes it easier to manage dependencies
+func NewRepository(app *config.AppConfig) *Repository {
+	return &Repository{app: app}
+}
+
+func (m *Repository) SignUp(c *gin.Context) {
+	var RequestPayload database.User
+	err := m.app.Models.Users.Insert(RequestPayload)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to sign up"})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"message": "User SignedUp successfully"})
+	c.Redirect(http.StatusSeeOther, "/user/signin")
+}
+
+func (m *Repository) Authenticate(c *gin.Context) {
+	var UserPayload struct {
+		Email    string `json:"email"`
+		Password string `json:"password"`
+	}
+	user, err := m.app.Models.Users.GetEmail(UserPayload.Email)
+	if err != nil {
+		log.Println("Invalid Email")
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "invalid credentials"})
+		return
+	}
+	valid, err := user.PasswordMatches(UserPayload.Password)
+	if err != nil || !valid {
+		log.Println("Invalid Password")
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "invalid credentials"})
+		return
+	}
+	// Initialize the session and set the userID
+	session := sessions.Default(c)
+	session.Set("userID", user.ID)
+	session.Save()
+	c.JSON(http.StatusOK, gin.H{"message": "User Authenticated successfully"})
+	log.Println("Authenticated")
+}
