03962361d8961354b9b338e58e81663d40d4d4b4
Philip-21 <philipuzomaobiora@gmail.com>
2023-11-30 12:46:48 +0100

    routes created
    Signed-off-by: Philip-21 <philipuzomaobiora@gmail.com>

---
+++ ContentDeliveryService/routes/routes.go
@@ -0,0 +1,24 @@
+package routes
+
+import (
+	"contentDelivery/handlers"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+)
+
+func Routes(api *handlers.Repository) *gin.Engine {
+	route := gin.Default()
+	route.Use(gin.Logger())
+	route.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"https//*", "http://*"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE"},
+		AllowHeaders:     []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposeHeaders:    []string{"Link"},
+		AllowCredentials: true,
+	}))
+	route.POST("/set",api.GetUserPreference)
+
+	return route
+
+}
